15a1953179c6afc9c5ac8ec83dd5d690e12c27b8
hzlucasf <hzlucasf@gmail.com>
2024-10-21 16:03:53 -0300

    Added std_err.h and std_err.c

---
+++ std_err/std_err.c
@@ -0,0 +1,5 @@
+#include "std_err.h"
+
+//
+
+u8 std_last_err_code = STD_NO_ERR;
---
+++ std_err/std_err.h
@@ -0,0 +1,16 @@
+#pragma once
+
+//
+
+#include "../std_int/std_int.h"
+
+//
+
+#define STD_NO_ERR           0
+#define STD_MEM_ALLOC_ERR    1
+#define STD_INVALID_ARGS_ERR 2
+#define STD_NOT_FOUND_ERR    3
+
+//
+
+extern u8 std_last_err_code;
